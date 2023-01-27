b68bddfe9a493f773300b53869f441ef02ddbe47
Paul Kleinschmidt <paul.kleinschmidt1@gmail.com>
2023-01-26 16:39:47 -0800

    Add restaurants controller, route, and rubocop

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  scope "/api" do
+    resources :restaurants do
+      collection do
+        get "search"
+      end
+    end
+  end
 end
