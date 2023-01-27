393e374ea2e6532f0ae7901e476ccfb3b8070faf
Paul Kleinschmidt <paul.kleinschmidt1@gmail.com>
2023-01-27 12:06:38 -0800

    Add restaurants search action

--- server/app/controllers/restaurants_controller.rb
+++ server/app/controllers/restaurants_controller.rb
@@ -1,4 +1,13 @@
 class RestaurantsController < ApplicationController
   def search
+    if params[:query]
+      @response = RestClient.get "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{params[:query]}&key=#{Rails.application.credentials.places_api_key}&type=restaurant",
+                                 { content_type: :json, accept: :json, "user-key": ENV["API_KEY"] }
+      @results = JSON.parse(@response.body)["results"]
+
+      render status: :ok, json: @results
+    else
+      render status: :bad_request
+    end
   end
 end
